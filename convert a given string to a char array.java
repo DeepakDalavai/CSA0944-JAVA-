562e39bfd452177eba126669cb351f9d8e182ea7
DeepakDalavai <129154884+DeepakDalavai@users.noreply.github.com>
2024-11-13 09:19:53 +0530

    Create convert a given string to a char array.java

---
+++ convert a given string to a char array.java
@@ -0,0 +1,10 @@
+public class R192210646 {
+    public static void main(String args[]) {
+        String str = "HelloWorld";
+        char[] charArray=str.toCharArray();
+        System.out.println("Character array:");
+        for (char c : charArray) {
+            System.out.print(c + " ");
+        }
+    }
+}
